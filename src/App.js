d96a7843497157e00f1017f4aaa78fc2ffe75427
Joebel Miniano <gomezjoebel@gmail.com>
2024-01-24 16:51:37 +0800

    jobz

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import bg from './assets/img/bg1.jpg';
+
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <body>
-        <h1>JOBZILLA!</h1>
-        </body>
+
+        <Home/>
+
       </header>
     </div>
   );
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import me from '../assets/img/jobzz.png';
+
+function home() {
+  return (
+    <div className="home">
+        <div className="land">
+        <img src={me} className="jobz-photo" alt="logo" />
+        </div>
+    </div>
+  )
+}
+
+export default home
