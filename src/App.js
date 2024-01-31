96247b1c64cabc02c52a2daa161210fea0ded54b
Joebel Miniano <gomezjoebel@gmail.com>
2024-02-01 00:11:33 +0800

    jobz

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-
 import Home from './pages/home';
 
 function App() {
+
   return (
     <div className='container1'>
+      
     <div className="App">
+      
       <header className="App-header">
-
+        
         <Home/>
 
       </header>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../assets/css/style.css'
 import Lottie from 'react-lottie';
 import * as animationData from '../assets/json/butt.json'
-import me from '../assets/img/jobz123.png';
+import logo from '../assets/img/jobz-w.png';
 
-function home() {
+function Home() {
 
   const defaultOptions = {
     loop: true,
-    autoplay: true, 
+    autoplay: true,
     animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
@@ -17,60 +17,60 @@ function home() {
 
   return (
 
-<div className="asdasd">
-<div className="loader">
-      <div className="loader1">
-      <Lottie options={defaultOptions}
-              height={100}
-              width={100}/>
-      </div>
+    <div className="container">
+      <div className="loader-relative">
+        <div className="loader">
+          <Lottie options={defaultOptions}
+            height={100}
+            width={100} />
+        </div>
       </div>
 
       <div className="container">
 
-      
+
 
         <div className="old">
-      
-      <div className="home">
 
-        <nav class="navbar navbar-expand-lg">
-          <div class="container-fluid">
-            {/* <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button> */}
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                <a class="nav-link" href="#">ABOUT ME</a>
-                <a class="nav-link" href="#">PROJECTS</a>
-                <a class="nav-link-hire" href="#">HIRE ME</a>
+          <div className="home">
+
+            <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+              <div className="photooo"><img src={logo} className="jobz-logo" alt="logo" style={{width: '100px'}} /></div>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                    <a class="nav-link" href="#">ABOUT ME</a>
+                    <a class="nav-link" href="#">PROJECTS</a>
+                    <a class="nav-link-hire" href="#">HIRE ME</a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </nav>
+            </nav>
 
-        <div className="home-content">
-          {/* <div className="photooo"><img src={me} className="jobz-photo" alt="logo" /></div> */}
-          <div className="land-txt" style={{textAlign: 'start'}}>
-          <h1>JOBZ</h1><br></br>
-          <h2 style={{color: 'orange', marginTop: '-37px'}}>WEB / GRAPHIC DESIGNER</h2>
+            <div className="home-content">
+              {/* <div className="photooo"><img src={me} className="jobz-photo" alt="logo" /></div> */}
+              <div className="land-txt" style={{ textAlign: 'start' }}>
+                <h1>JOBZ</h1><br></br>
+                <h2 style={{ color: 'orange', marginTop: '-37px' }}>WEB / GRAPHIC DESIGNER</h2>
+              </div>
+
+
+            </div>
           </div>
 
 
         </div>
+
       </div>
 
-      
-    </div>
-    
-    </div>
-</div>
-    
 
-    
+    </div>
+
+  
+
+
   )
 }
 
-export default home
+export default Home
