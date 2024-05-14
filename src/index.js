ef5f945df872ce930df9eeb6a4171cf405a21a8f
xennon <gameoveralisa@gmail.com>
2024-05-14 15:33:50 +0800

    Added router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import TestSeparatePage from './pages/test-separate-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <TestSeparatePage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/test-separate-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TestSeparatePage = () => {
+  return (
+    <div style={{ color: 'white', fontSize: '40px' }}>
+      <div className="">Test Separate Page</div>
+      <div className="" style={{ fontWeight: 'bold'}}>This file is located in "/src/pages/test-separate-page.jsx"</div>
+      <div className="" style={{ fontWeight: 'bold'}}>To add new page, open: "/src/index.js"</div>
+      
+    </div>
+  )
+}
+
+export default TestSeparatePage
